bf987dc5e0e270888b375befb601e9fd065386a0
EC2 Default User <ec2-user@ip-10-0-0-200.ap-southeast-2.compute.internal>
2020-07-05 13:04:22 +0000

    2.2.1. VPC

--- eks-cluster/lib/eks-cluster-stack.ts
+++ eks-cluster/lib/eks-cluster-stack.ts
@@ -1,9 +1,55 @@
 import * as cdk from '@aws-cdk/core';
 
+import * as dotenv from 'dotenv';
+import * as ec2 from '@aws-cdk/aws-ec2';
+
 export class EksClusterStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    
+    dotenv.config();
+
+    /**
+     * Step 1. use an existing VPC or create a new one for our EKS Cluster
+     */  
+    const vpc = getOrCreateVpc(this);
+    
   }
+}
+
+
+/**
+ * Step 1. use an existing VPC or create a new one for our EKS Cluster
+ * 
+ * Note: only 1 NAT Gateway --> Cost Optimization trade-off
+ */ 
+function getOrCreateVpc(stack: cdk.Stack): ec2.IVpc {
+  
+  var vpc_name = process.env.AWS_VPC_NAME || "EKS-VPC";
+  var vpc_cidr = process.env.AWS_VPC_CIDR || "10.10.0.0/18";
+  // console.log(`vpc_name is ${process.env.AWS_VPC_NAME}`);
+  // console.log(`vpc_cidr is ${process.env.AWS_VPC_CIDR}`);
+  // console.log(`vpc_name is ${process.env.AWS_VPC_MAX_AZ}`);
+  // console.log(`vpc_cidr is ${process.env.AWS_VPC_NAT_GATEWAY}`);
+  
+  /** Use an existing VPC or create a new one */
+  const vpc = stack.node.tryGetContext('use_default_vpc') === '1' ?
+    ec2.Vpc.fromLookup(stack, vpc_name, { isDefault: true }) :
+    stack.node.tryGetContext('use_vpc_id') ?
+      ec2.Vpc.fromLookup(stack, vpc_name, 
+        { vpcId: stack.node.tryGetContext('use_vpc_id') }) :
+          new ec2.Vpc(stack, vpc_name, 
+            { cidr: vpc_cidr,
+              maxAzs: 2,
+              natGateways: 1,
+              subnetConfiguration: [
+                { cidrMask: 24, subnetType: ec2.SubnetType.PUBLIC,  
+                  name: "PublicDMZ"  },
+                { cidrMask: 24, subnetType: ec2.SubnetType.PRIVATE, 
+                  name: "PrivateServices" } ]
+            });  
+      
+  return vpc
 }

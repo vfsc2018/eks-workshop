7572c0dea1fa7bf1ab688af46bdd572bd0b809b8
Thanh Nguyen <nnthanh101@gmail.com>
2020-07-08 15:20:59 +0700

    ⚡ 3.1. CDK8s with TypeScript

--- podinfo/main.ts
+++ podinfo/main.ts
@@ -1,11 +1,46 @@
 import { Construct } from 'constructs';
 import { App, Chart } from 'cdk8s';
+/** imported constructs */
+import { Deployment, Service, IntOrString } from './imports/k8s';
+// import { Deployment, Service } from "../lib";
+import { PodinfoContainer } from "./podinfo";
 
 export class MyChart extends Chart {
   constructor(scope: Construct, name: string) {
     super(scope, name);
 
     // define resources here
+    const label = { app: 'podinfo-k8s' };
+
+    /** Deploys hello-kubernetes as a Service behind a LoadBalancer */
+    new Service(this, 'service', {
+      spec: {
+        type: 'LoadBalancer',
+        ports: [ { port: 80, targetPort: IntOrString.fromNumber(8080) } ],
+        selector: label
+      }
+    });
+
+    new Deployment(this, 'deployment', {
+      spec: {
+        replicas: 2,
+        selector: {
+          matchLabels: label
+        },
+        template: {
+          metadata: { labels: label },
+          spec: {
+            containers: [
+              {
+                name: 'podinfo-kubernetes',
+                image: 'paulbouwer/hello-kubernetes:1.8',
+                ports: [ { containerPort: 8080 } ]
+              }
+            ]
+          }
+        }
+      }
+    });
 
   }
 }
